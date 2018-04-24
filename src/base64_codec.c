d993753de3a3c15adb79c19fca8e06c4c3a938e8
Sean Caron <scaron@umich.edu>
2018-04-23 23:34:00 -0400

    Style cleanup

--- src/base64_codec.c
+++ src/base64_codec.c
@@ -4,140 +4,127 @@
 #include "base64.h"
 
 int base64_encode(char *ip, char **op) {
+    unsigned long r = 0;
 
-	unsigned long r = 0;
+    int pi = 0;
+    int po = 0;
 
-	int pi = 0;
-	int po = 0;
+    *op = malloc(5*sizeof(char));
 
-	*op = malloc( 5*sizeof(char) );
+    if (*op == 0) {
+        exits("MALLOC");
+    }
 
-	if ( *op == 0 ) {
-		exits("MALLOC");
-	}
+    for (pi = 0; pi < strlen(ip)+1; pi = pi + 3) {
+        if (ip[pi] == '\0') {
+            *(*op + (4*po)) = '\0';
+            break;
+        }
 
-	for ( pi = 0; pi < strlen(ip)+1; pi = pi + 3) {
+        else if (ip[pi+1] == '\0') {
+            // two characters output, == pad
+            r = (ip[pi] << 24);
 
-		if ( ip[pi] == '\0') {
+            *(*op + (4*po)) = base64_lut[r >> 26];
+            *(*op + ((4*po)+1)) = base64_lut[(r << 6) >> 26];
+            *(*op + ((4*po)+2)) = '=';
+            *(*op + ((4*po)+3)) = '=';
 
-			*(*op + (4*po)) = '\0';
-			break;
-		}
+            *(*op + ((4*po)+4)) = '\0';
 
-		else if ( ip[pi+1] == '\0' ) {
+            break;
+        }
 
-			// two characters output, == pad
+        else if (ip[pi+2] == '\0') {
+            // three characters output, = pad
 
-			r = (ip[pi] << 24);
+            r = (ip[pi] << 24) | (ip[pi+1] << 16);
 
-			*(*op + (4*po)) = base64_lut[r >> 26];
-			*(*op + ((4*po)+1)) = base64_lut[(r << 6) >> 26];
-			*(*op + ((4*po)+2)) = '=';
-			*(*op + ((4*po)+3)) = '=';
+            *(*op + (4*po)) = base64_lut[r >> 26];
+            *(*op + ((4*po)+1)) = base64_lut[(r << 6) >> 26];
+            *(*op + ((4*po)+2)) = base64_lut[(r << 12) >> 26];
+            *(*op + ((4*po)+3)) = '=';
 
-			*(*op + ((4*po)+4)) = '\0';
+            *(*op + ((4*po)+4)) = '\0';
 
-			break;
-		}
+            break;
+        }
 
-		else if ( ip[pi+2] == '\0' ) {
+        else {
+            // four characters output, no pad
+            r = (ip[pi] << 24) | (ip[pi+1] << 16) | (ip[pi+2] << 8);
 
-			// three characters output, = pad
+            *(*op + (4*po)) = base64_lut[r >> 26];
+            *(*op + ((4*po)+1)) = base64_lut[(r << 6) >> 26];
+            *(*op + ((4*po)+2)) = base64_lut[(r << 12) >> 26];
+            *(*op + ((4*po)+3)) = base64_lut[(r << 18) >> 26];
+        }
 
-			r = (ip[pi] << 24) | (ip[pi+1] << 16);
+        po = po + 1;
 
-			*(*op + (4*po)) = base64_lut[r >> 26];
-			*(*op + ((4*po)+1)) = base64_lut[(r << 6) >> 26];
-			*(*op + ((4*po)+2)) = base64_lut[(r << 12) >> 26];
-			*(*op + ((4*po)+3)) = '=';
+        *op = realloc(*op, sizeof(char)*((4*po)+5) );
 
-			*(*op + ((4*po)+4)) = '\0';
+        if ( *op == 0 ) {
+            exits("REALLOC");
+        }
+    }
 
-			break;
-		}
-
-		else {
-
-			// four characters output, no pad
-
-			r = (ip[pi] << 24) | (ip[pi+1] << 16) | (ip[pi+2] << 8);
-
-			*(*op + (4*po)) = base64_lut[r >> 26];
-			*(*op + ((4*po)+1)) = base64_lut[(r << 6) >> 26];
-			*(*op + ((4*po)+2)) = base64_lut[(r << 12) >> 26];
-			*(*op + ((4*po)+3)) = base64_lut[(r << 18) >> 26];
-		}
-
-		po = po + 1;
-
-		*op = realloc(*op, sizeof(char)*((4*po)+5) );
-
-		if ( *op == 0 ) {
-			exits("REALLOC");
-		}
-
-	}
-	
-	return strlen(*op);
+    return strlen(*op);
 }
 
 int base64_decode(char *ip, char **op) {
+    unsigned long r = 0;
+    unsigned char t[4] = {0, 0, 0, 0};
 
-	unsigned long r = 0;
+    int pi = 0;
+    int po = 0;
 
-	unsigned char t[4] = {0, 0, 0, 0};
+    /* return with -1 to signal malformed input */
 
-	int pi = 0;
-	int po = 0;
+    if ((strlen(ip) % 4) != 0 ) {
+        return -1;
+    }
 
-	/* return with -1 to signal malformed input */
+    *op = malloc( 4*sizeof(char) );
 
-	if ( (strlen(ip) % 4) != 0 ) {
-		return -1;
-	}
+    if (*op == 0) {
+        exits("MALLOC");
+    }
 
-	*op = malloc( 4*sizeof(char) );
+    for (pi = 0; pi < strlen(ip); pi = pi + 4) {
+        t[0] = base64_ilut[ip[pi]] << 2;
+        t[1] = base64_ilut[ip[pi+1]] << 2;
+        t[2] = base64_ilut[ip[pi+2]] << 2;
+        t[3] = base64_ilut[ip[pi+3]] << 2;
 
-	if ( *op == 0 ) {
-		exits("MALLOC");
-	}
+        r = t[0] << 24;
+        r = r & 0xFC000000;
 
-	for ( pi = 0; pi < strlen(ip); pi = pi + 4) {
+        r = r | (t[1] << 18);
+        r = r & 0xFFF00000;
 
-		t[0] = base64_ilut[ip[pi]] << 2;
-		t[1] = base64_ilut[ip[pi+1]] << 2;
-		t[2] = base64_ilut[ip[pi+2]] << 2;
-		t[3] = base64_ilut[ip[pi+3]] << 2;
+        r = r | (t[2] << 12);
+        r = r & 0xFFFFC000;
 
-		r = t[0] << 24;
-		r = r & 0xFC000000;
+        r = r | (t[3] << 6);
+        r = r & 0xFFFFFF00;
 
-		r = r | (t[1] << 18);
-		r = r & 0xFFF00000;
+	/* the characters are packed in the three high order bytes of long, r. the lower 8 bits of r are zero */
 
-		r = r | (t[2] << 12);
-		r = r & 0xFFFFC000;
+        *(*op + (3*po)) = (char)(r>>24);
+        *(*op + ((3*po)+1)) = (char)((r<<8)>>24);
+        *(*op + ((3*po)+2)) = (char)((r<<16)>>24);
 
-		r = r | (t[3] << 6);
-		r = r & 0xFFFFFF00;
+        *(*op + ((3*po)+3)) = '\0';
 
-		/* the characters are packed in the three high order bytes of long, r. the lower 8 bits of r are zero */
+        po = po + 1;
 
-		*(*op + (3*po)) = (char)(r>>24);
-		*(*op + ((3*po)+1)) = (char)((r<<8)>>24);
-		*(*op + ((3*po)+2)) = (char)((r<<16)>>24);
+        *op = realloc(*op, sizeof(char)*((4*po)+5) );
 
-		*(*op + ((3*po)+3)) = '\0';
+        if (*op == 0) {
+            exits("REALLOC");
+        }
+    }
 
-		po = po + 1;
-
-		*op = realloc(*op, sizeof(char)*((4*po)+5) );
-
-		if ( *op == 0 ) {
-			exits("REALLOC");
-		}
-
-	}
-
-	return strlen(*op);
+    return strlen(*op);
 }
