481481737ab1ef25811385f47007fdd4084ea278
Sean Caron <scaron@umich.edu>
2018-04-24 21:54:21 -0400

    Style cleanup

--- src/uri_codec.c
+++ src/uri_codec.c
@@ -3,129 +3,113 @@
 #include "main.h"
 
 int uri_encode(char *ip, char **op) {
+    int i, j, t;
+    char temp[4];
+    int ol;
 
-	int i, j, t;
-	char temp[4];
-	int ol;
+    ol = strlen(ip);
 
-	ol = strlen(ip);
+    *op = malloc ( sizeof(char) * (ol+1) );
 
-	*op = malloc ( sizeof(char) * (ol+1) );
+    if (*op == 0) {
+        exits("MALLOC");
+    }
 
-	if ( *op == 0 ) {
-		exits("MALLOC");
-	}
+    i = 0;
+    j = 0;
 
-	i = 0;
-	j = 0;
+    /* catch null input */
+    if (strlen(ip) == 0) {
+        strcpy(*op, "");
+        return 0;
+    }
 
-	/* catch null input */
+    while (1) {
+        /* just pass unreserved characters through with no encoding */
 
-	if ( strlen(ip) == 0 ) {
-		strcpy(*op, "");
-		return 0;
-	}
+        if (((ip[i] >= 43) && (ip[i] <= 47)) || (ip[i] == 95) || (ip[i] == 126) || ((ip[i] >= 48) && (ip[i] <= 57)) || ((ip[i] >= 65) && (ip[i] <= 90)) || ((ip[i] >= 97) && (ip[i] <= 122))) {
+            *(*op+j) = ip[i];
 
-	while (1) {
+            i = i + 1;
+            j = j + 1;
+        }
 
-		/* just pass unreserved characters through with no encoding */
+        /* otherwise express it in the uri encoded format */
+        else {
+            ol = ol + 3;
 
-		if ( ((ip[i] >= 43) && (ip[i] <= 47)) || (ip[i] == 95) || (ip[i] == 126) || ( (ip[i] >= 48) && (ip[i] <= 57) ) || ( (ip[i] >= 65) && (ip[i] <= 90) ) || ( (ip[i] >= 97) && (ip[i] <= 122) ) ) {
+            *op = realloc(*op, sizeof(char)*(ol+1) );
 
-			*(*op+j) = ip[i];
+            if (*op == 0) {
+                exits("REALLOC");
+            }
 
-			i = i + 1;
-			j = j + 1;
-		}
+            sprintf(temp, "%2.0x", ip[i] );
 
-		/* otherwise express it in the uri encoded format */
+            *(*op+j) = '%';
+            *(*op+j+1) = temp[0];
+            *(*op+j+2) = temp[1];
 
-		else {
+            i = i + 1;
+            j = j + 3;
+        }
 
-			ol = ol + 3;
+        if (i == strlen(ip)) {
+            break;
+        }
+    }
 
-			*op = realloc(*op, sizeof(char)*(ol+1) );
+    *(*op+j) = '\0';
 
-			if ( *op == 0 ) {
-				exits("REALLOC");
-			}
-
-			sprintf(temp, "%2.0x", ip[i] );
-
-			*(*op+j) = '%';
-			*(*op+j+1) = temp[0];
-			*(*op+j+2) = temp[1];
-
-			i = i + 1;
-			j = j + 3;
-		}
-
-		if ( i == strlen(ip) ) {
-			break;
-		}
-
-	}
-
-	*(*op+j) = '\0';
-
-	return 0;
-	
+    return 0;
 }
 
 int uri_decode(char *ip, char **op) {
+    int i, j, t;
+    char temp[4];
 
-	int i, j, t;
-	char temp[4];
+    *op = malloc ( sizeof(char) * (strlen(ip)+1) );
 
-	*op = malloc ( sizeof(char) * (strlen(ip)+1) );
+    if (*op == 0) {
+        exits("MALLOC");
+    }
 
-	if ( *op == 0 ) {
-		exits("MALLOC");
-	}
+    i = 0;
+    j = 0;
 
-	i = 0;
-	j = 0;
+    /* catch null input */
+    if (strlen(ip) == 0) {
+        strcpy(*op, "");
+        return 0;
+    }
 
-	/* catch null input */
+    while (1) {
+        if ((ip[i] == '%') && isxdigit(ip[i+1]) && isxdigit(ip[i+2])) {
+            // ascii value of the encoded char is in ip[i+1] and ip[i+2] in hex; ip[i+1] is MSB.
+            sprintf(temp, "%c%c", ip[i+1], ip[i+2] );
 
-	if ( strlen(ip) == 0 ) {
-		strcpy(*op, "");
-		return 0;
-	}
+            t = (int)strtol(temp, (char**)NULL, 16);
 
-	while (1) {
+            *(*op+j) = (char)t;
 
-		if ( (ip[i] == '%') && isxdigit(ip[i+1]) && isxdigit(ip[i+2]) ) {
+            i = i + 3;
+            j = j + 1;
+        }
 
-			// ascii value of the encoded char is in ip[i+1] and ip[i+2] in hex; ip[i+1] is MSB.
+        else {
+            *(*op+j) = ip[i];
 
-			sprintf(temp, "%c%c", ip[i+1], ip[i+2] );
+            i = i + 1;
+            j = j + 1;
+        }
 
-			t = (int)strtol(temp, (char**)NULL, 16);
+        if (i == strlen(ip)) {
+            break;
+        }
+    }
 
-			*(*op+j) = (char)t;
+    *(*op+j) = '\0';
 
-			i = i + 3;
-			j = j + 1;
-		}
-
-		else {
-
-			*(*op+j) = ip[i];
-
-			i = i + 1;
-			j = j + 1;
-
-		}
-
-		if ( i == strlen(ip) ) {
-			break;
-		}
-
-	}
-
-	*(*op+j) = '\0';
-
-	return 0;
-	
+    return 0;
 }
+
