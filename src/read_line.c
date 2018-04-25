390cfcd2c4620b3b3e051b3c3dcc872d2a8efd6b
Sean Caron <scaron@umich.edu>
2018-04-24 21:38:30 -0400

    Clean up style

--- src/read_line.c
+++ src/read_line.c
@@ -2,39 +2,35 @@
 
 #include "main.h"
 
-/* read in a line from the specified file descriptor to the specified buffer */
-
+/* Read in a line from the specified file descriptor to the specified buffer */
 int read_line(int infd, char **ptr) {
+    int len = 1;
+    int n;
 
-	int len = 1;
-	int n;
+    *ptr = malloc(sizeof(char));
 
-	*ptr = malloc(sizeof(char));
+    while (1) {
+        n = read(infd, &(*(*ptr+len-1)), 1);
 
-	while (1) {
+        *ptr = realloc(*ptr, (len+1)*sizeof(char));
 
-		n = read(infd, &(*(*ptr+len-1)), 1);
+        if (*ptr == 0) {
+            exits("REALLOC");
+        }
 
-		*ptr = realloc(*ptr, (len+1)*sizeof(char));
+        if (*(*ptr+len-1) == 10) {
+            *(*ptr+len) = '\0';
+            break;
+        }
 
-		if ( *ptr == 0 ) {
-			exits("REALLOC");
-		}
+        /* Disconnected */
+        if (n == 0) {
+            exits("DISCONNECTED");
+        }
 
-		if ( *(*ptr+len-1) == 10 ) {
-			*(*ptr+len) = '\0';
-			break;
-		}
+        len = len + 1;
+    }
 
-		/* disconnected */
-
-		if ( n == 0 ) {
-			exits("DISCONNECTED");
-		}
-
-		len = len + 1;
-
-	}
-
-	return len;
+    return len;
 }
+
